404890b9609500bd1274d0166aebeec9dda6ce7a
andrerodrigues <andrerodrigues29101992@gmail.com>
2023-11-28 01:21:30 +0000

    fix: metrics env file

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,14 +3,14 @@ import AppHead from "./AppHead";
 import Footer from "./Footer";
 import Navbar from "./Navbar/Navbar";
 import { init, trackPages } from "insights-js";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import Loading from "./Loading/Loading";
 
 export default function Layout({ children }) {
   const isMobile = useIsMobile();
 
   useEffect(() => {
-    init(process.env.METRICS_ID);
+    init(process.env.NEXT_PUBLIC_METRICS_ID);
     trackPages();
   }, []);
 
